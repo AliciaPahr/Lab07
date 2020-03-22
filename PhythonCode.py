a8fd5f3e74c22c3368980bfca14d95796a6811d5
AliciaPahr <62497028+AliciaPahr@users.noreply.github.com>
2020-03-22 04:59:51 -0400

    Lab7 Phython Code

---
+++ PhythonCode.py
@@ -0,0 +1,6 @@
+userName = input ("Hello! Please enter your facebook username!")
+userPassword = input("Hello! Please enter your facebook password!")
+if(userName.lower() == "alicia" and userPassword == "csf534"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
